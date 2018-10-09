21741924311849e9025e8572dab2525e0c536bea
luantran96 <luantran96@yahoo.com>
2018-10-08 19:56:58 -0700

    Finish up to promisification

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,29 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
   // TODO
+  fs.readFile(filePath, (err, data) => {
+    
+    if (err) {
+      callback(err, null);
+    } else {      
+      var dataText = data.toString().split('\n');
+      callback(err, dataText[0]);
+    }   
+  });  
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request(url, (err, response, body) => {   
+    if (!err) {      
+     var statusCode = response.statusCode;   
+      callback(err, response.statusCode);           
+    } else {
+      callback(err, null);
+    }
+  }); 
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,30 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  const myFirstPromise = new Promise((resolve, reject) => {    
+    fs.readFile(filePath, (err, data) => { 
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data.toString().split('\n')[0]);
+        }
+    });  
+  }); 
+  return myFirstPromise; 
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  const myFirstPromise = new Promise((resolve, reject) => {
+    request(url, (err, response, body) => {   
+      if (!err) {          
+        resolve(response.statusCode);           
+      } else {
+        reject(err);
+      }    
+    });       
+  });  
+  return myFirstPromise;
 };
 
 // Export these functions so we can test them and reuse them in later exercises
